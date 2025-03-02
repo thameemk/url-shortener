3cde7e84b7737cab0ec44e6a8c4b5b8f170d3f00
thameemk <33159840+thameemk@users.noreply.github.com>
2025-03-02 22:14:46 +0530

    Add: axum web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // Define routes
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    // Start server
+    let listener = TcpListener::bind("0.0.0.0:8000")
+        .await
+        .expect("Failed to bind port");
+    println!("🚀 Server running on http://127.0.0.1:8000");
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server");
 }
